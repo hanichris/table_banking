4ed9e8e087743ea1fad7118ad9f4a49819a48bf1
hanichris <hanichris71@gmail.com>
2023-08-07 18:36:40 +0000

    Build out the scaffloading of the `Table` models.

--- app/backend/app/schemas/table.py
+++ app/backend/app/schemas/table.py
@@ -4,17 +4,36 @@
 These models will parse the data providing validation and conversion
 (whenever necessary.)
 """
+from decimal import Decimal
+
 from pydantic import BaseModel, ConfigDict
+
+from user import User
 
 
 class TableBase(BaseModel):
-    pass
+    """Model defining the common base attributes of a `table` bank."""
+    title: str
+    interest_rate: Decimal = Decimal(0)
+    amount: Decimal = Decimal(0)
 
 class TableCreate(TableBase):
-    pass
+    """Model defining properties to receive on `table` creation."""
+    admin_id: int
 
 class TableUpdate(TableBase):
-    pass
+    """Model defining the properties to receive on `table` update."""
+    amount_loaned_out: Decimal
 
 class Table(TableBase):
+    id: int
+    admin_id: int
+
     model_config = ConfigDict(from_attributes=True)
+
+class TableUsers(Table):
+    """Model defining the users who are apart of a `table`.
+
+    Avoids the creation of a circular dependency.
+    """
+    users: list[User] = []
