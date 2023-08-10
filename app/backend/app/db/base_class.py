26aac91603c706145f4401a05ed52eed16f1d044
hanichris <hanichris71@gmail.com>
2023-08-10 18:32:41 +0000

    create an association table to facilitate the generation of a `many-to-many` relationship

--- app/backend/app/db/base_class.py
+++ app/backend/app/db/base_class.py
@@ -5,8 +5,18 @@ With the declarative base class, new mapped classes are declared as subclasses
 of the base.
 https://docs.sqlalchemy.org/en/20/orm/declarative_styles.html
 """
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 
 class Base(DeclarativeBase):
-    pass
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+association_table = Table(
+    'association_table',
+    Base.metadata,
+    Column('left_id', ForeignKey('user.id')),
+    Column('right_id', ForeignKey('bank.id')),
+)
