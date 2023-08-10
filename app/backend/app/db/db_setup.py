a817bc8c70183ab04c753a23db9e9fca2557fd40
hanichris <hanichris71@gmail.com>
2023-08-10 17:24:11 +0000

    Setup the databasae scaffolding by creating the engine and sessionmaker

---
+++ app/backend/app/db/db_setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Configure the database use with SQLAlchemy."""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.settings_config import settings
+
+
+engine = create_engine(settings.SQL_ALCHEMY_DATABASE_URI,
+                       connect_args= {'check_same_thread': False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
