ab36ff62f65fa92ea99c38663adb5d0f027d40c1
hanichris <hanichris71@gmail.com>
2023-08-12 20:30:07 +0000

    Define CRUD operations for a base SQLAlchemy class

---
+++ app/backend/app/crud/base.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Define the base class for CRUD operations."""
+from typing import Generic, TypeVar
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.db.base_class import Base
+
+
+ModelType = TypeVar('ModelType', bound=Base)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    """Base class for CRUD operations that inherits from the Generic base class.
+
+    The `Generic` class which this class inherits from has been parameterized
+    by three type variables - `ModelType`, `CreateSchemaType` and
+    `UpdateSchemaType`.
+    """
+    def __init__(self, model: type[ModelType]) -> None:
+        """CRUD object with default methods to Create, Read, Update & Delete.
+
+        Args:
+            * `model`: A SQLAlchemy model class.
+        """
+        self.model = model
+
+    def get(self, db: Session, /, *, id: int) -> ModelType | None:
+        return db.get(self.model, id)
+
+    def get_multi(
+            self, db: Session, /, *, skip: int = 0, limit: int = 100
+            ) -> list[ModelType]:
+        stmt = select(self.model).offset(skip).limit(limit)
+        return db.execute(stmt).scalars().unique().all()
+
+    def create(
+            self, db: Session, /, *, obj_in: CreateSchemaType
+            ) -> ModelType:
+        data = jsonable_encoder(obj_in)
+        db_obj = self.model(**data)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
