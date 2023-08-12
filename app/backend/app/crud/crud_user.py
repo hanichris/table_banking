b41a9728edbc6ff21a3643bfa58aeb791826390f
hanichris <hanichris71@gmail.com>
2023-08-12 22:05:11 +0000

    Implement the `get_user_by_email` method in CRUDUser to enable query the database backend with the email of a user.

--- app/backend/app/crud/crud_user.py
+++ app/backend/app/crud/crud_user.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """Define CRUD operations for SQLAlchemy class `User`."""
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.core.security import get_password_hash
+from .base import CRUDBase
+from app.models.user import User
+from app.schemas import UserCreate, UserUpdate
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+    """Extends `CRUDBase` class to provide user specific CRUD operations."""
+    def get_by_email(self, db: Session, /, *, email: str) -> User | None:
+        return db.execute(select(User).filter_by(email=email)).scalar_one_or_none()
+
+user = CRUDUser(User)
