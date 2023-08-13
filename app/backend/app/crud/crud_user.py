89cee595b9cbf60f37d640c65dc92cd660b011f6
hanichris <hanichris71@gmail.com>
2023-08-13 19:40:11 +0000

    Implement the `create_user`, `is_active` and `is_superuser` methods for the `CRUDUser` class

--- app/backend/app/crud/crud_user.py
+++ app/backend/app/crud/crud_user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.core.security import get_password_hash
 from .base import CRUDBase
-from app.models.user import User
+from app.models import User
 from app.schemas import UserCreate, UserUpdate
 
 
@@ -13,5 +13,21 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     """Extends `CRUDBase` class to provide user specific CRUD operations."""
     def get_by_email(self, db: Session, /, *, email: str) -> User | None:
         return db.execute(select(User).filter_by(email=email)).scalar_one_or_none()
+
+    def create_user(self, db: Session, /, *, obj_in: UserCreate) -> User:
+        obj_in.password = get_password_hash(obj_in.password)
+        return super().create(db, obj_in=obj_in)
+
+    def update_user():
+        pass
+
+    def authenticate():
+        pass
+
+    def is_active(self, user: User) -> bool:
+        return user.is_active
+
+    def is_superuser(self, user: User) -> bool:
+        return user.is_superuser
 
 user = CRUDUser(User)
