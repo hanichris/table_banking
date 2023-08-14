11dc81db3c6b89bb198533df7625f8610f319ee1
hanichris <hanichris71@gmail.com>
2023-08-14 22:40:40 +0000

    Enforce unique constraint on the association table through the primary key constraint to ensure duplicate rows arent persisted to the database

--- app/backend/app/db/base_class.py
+++ app/backend/app/db/base_class.py
@@ -4,6 +4,7 @@
 With the declarative base class, new mapped classes are declared as subclasses
 of the base.
 https://docs.sqlalchemy.org/en/20/orm/declarative_styles.html
+https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html#many-to-many
 """
 from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import DeclarativeBase, declared_attr
@@ -14,9 +15,13 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
 
+# To create a simple many-to-many relationship, we setup an association/junction
+# table. It is recommended to that the two columns referring to the two entity
+# tables are established with a unique constraint or primary key constraint
+# to ensure that duplicate rows aren't persisted.
 association_table = Table(
     'association_table',
     Base.metadata,
-    Column('left_id', ForeignKey('user.id')),
-    Column('right_id', ForeignKey('bank.id')),
+    Column('left_id', ForeignKey('user.id'), primary_key=True),
+    Column('right_id', ForeignKey('bank.id'), primary_key=True),
 )
