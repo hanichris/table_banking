aa295359db43e7f11c8342e5b36afdeb13482ad4
hanichris <hanichris71@gmail.com>
2023-08-16 21:46:47 +0000

    Include the `bank` router in the pool of avalilable routers.

--- app/backend/app/api/v1/api.py
+++ app/backend/app/api/v1/api.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from fastapi import APIRouter
 
-from .routers import login, users
+from .routers import banks, login, users
 
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
+api_router.include_router(banks.router)
