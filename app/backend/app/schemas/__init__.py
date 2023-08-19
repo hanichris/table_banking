50b2e8fb604fdd6f93048e3d1f6b558a7b2a1085
hanichris <hanichris71@gmail.com>
2023-08-19 22:54:36 +0000

    Move `Msg` class to the subpackage level

--- app/backend/app/schemas/__init__.py
+++ app/backend/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .bank import  BankCreate, BankUpdate, Bank
 from .helper import BankUsers, UserBanks
+from .msg import Msg
 from .token import Token, TokenPayload
 from .user import UserCreate, UserUpdate, User, UserInDB
