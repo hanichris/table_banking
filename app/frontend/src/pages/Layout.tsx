80d03dbfabb9b6a2dc71b8bb693ca9a0342bac95
hanichris <hanichris71@gmail.com>
2023-08-22 23:12:51 +0000

    Refactor import name

--- app/frontend/src/pages/Layout.tsx
+++ app/frontend/src/pages/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import NavBar from "../components/SideBar";
+import NavBar from "../components/NavBar";
 
 export default function Layout() {
     return (
