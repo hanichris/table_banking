55a2173cb74e407ea9e1c581cd51c8d966c83c37
hanichris <hanichris71@gmail.com>
2023-08-22 23:01:26 +0000

    Component dealing with the SideBar UI

---
+++ app/frontend/src/components/SideBar.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
