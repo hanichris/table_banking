71245d6b02163963d8c2a6c49c6cdb8b0e4ed1a7
hanichris <hanichris71@gmail.com>
2023-08-23 23:10:51 +0000

    Reorder stylesheet imports

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
 
 // Perform / trigger the inital render.
 // It targets the target DOM node referenced by the id `root`.
