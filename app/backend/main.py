3496e66869e688ef65077af4d20afc28ce59dbf3
hanichris <hanichris71@gmail.com>
2023-08-26 22:53:22 +0000

    Include CORSMiddleware to configure CORS ruleset.
    Utilise environment variable to provide a list of acceptable request origins.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Main application."""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 from app.core.security import pwd_context
@@ -12,5 +13,14 @@ app = FastAPI(
     openapi_url=f'{settings.API_V1_STR}/openapi.json',
     )
 pwd_context.load_path(settings.SECURITY_CONIFIG_FILE)
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
