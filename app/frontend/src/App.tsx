5903f2ca1cd376dd9eca980e6b35270ad49585ff
hanichris <hanichris71@gmail.com>
2023-08-26 22:54:19 +0000

    Test out funtionality to launch 3 different modals for authentication/authorization.

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -4,18 +4,62 @@
 // import Layout from './pages/Layout';
 // import NoPage from './pages/NoPage';
 import { useState } from 'react';
-// import UserForm from './components/UserForm';
+
+import UserForm from './components/UserForm';
 import SignIn from "./components/modals/signin";
+import SignUp from './components/modals/signup';
 
 import './App.css'
 
+interface Auth {
+  state: string,
+  form: boolean,
+}
+
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  return (
-    <main>
-      <button onClick={() => setIsOpen(true)}>Sign in</button>
-      {isOpen && <SignIn isOpen={isOpen} setIsOpen={setIsOpen}/>}
-    </main>
+  const [status, setStatus] = useState('');
+  const [userForm, setUserForm] = useState(false);
+
+  function handleSetStatus(form: Auth ) {
+    setStatus(form.state);
+    setUserForm(form.form);
+  }
+
+  if (status === '') {
+    return (
+      <main>
+        <button onClick={() => handleSetStatus({form: true, state: 'signIn'})}>Sign In</button>
+        <button onClick={() => handleSetStatus({form: true, state: 'signUp'})}>Sign Up</button>
+      </main>
+    );
+  } else if (status === 'signIn') {
+    return (
+      <main>
+        <button onClick={() => handleSetStatus({form: true, state: 'signIn'})}>Sign In</button>
+        <button onClick={() => handleSetStatus({form: true, state: 'signUp'})}>Sign Up</button>
+        {userForm && <UserForm status={status} setIsOpen={setIsOpen} setUserForm={setUserForm}/>}
+        {(isOpen && status === 'signIn') && <SignIn isOpen={isOpen} setIsOpen={setIsOpen} setStatus={setStatus}/>}
+      </main>
+    );
+  } else if (status === 'signUp') {
+    return (
+      <main>
+        <button onClick={() => handleSetStatus({form: true, state: 'signIn'})}>Sign In</button>
+        <button onClick={() => handleSetStatus({form: true, state: 'signUp'})}>Sign Up</button>
+        {userForm && <UserForm status={status} setIsOpen={setIsOpen} setUserForm={setUserForm}/>}
+        {(isOpen && status === 'signUp') && <SignUp isOpen={isOpen} setIsOpen={setIsOpen} setStatus={setStatus}/>}
+      </main>
+    );
+  }
+
+  // return (
+  //   <main>
+  //     <button onClick={() => setStatus('signIn')}>Sign in</button>
+  //     <button onClick={() => setStatus('signUp')}>Sign up</button>
+  //     {status === 'signIn' ? <UserForm status={status} setStatus={setStatus}/> : ''}
+  //     {status === 'signUp' ? <UserForm status={status} setStatus={setStatus}/> : ''}
+  //   </main>
     // <BrowserRouter>
     //   <Routes>
     //     <Route path='' element={ <Layout/> }>
@@ -24,7 +68,7 @@ function App() {
     //     </Route>
     //   </Routes>
     // </BrowserRouter>
-  );
+  // );
 }
 
 export default App
