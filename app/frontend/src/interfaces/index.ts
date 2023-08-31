a90d5696f12e7344e15b563c62201af67347d8b1
hanichris <hanichris71@gmail.com>
2023-08-31 21:53:36 +0000

    Define interfaces for token endpoint and user profile endpoint.

---
+++ app/frontend/src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface IToken {
+  access_token: string,
+  token_type:string,
+}
+
+export interface IUserProfile {
+  email: string,
+  is_active: boolean,
+  is_superuser: boolean,
+  full_name: string,
+  id: number,
+  banks?: Array<object>,
+  banks_admin?: Array<object>,
+}
