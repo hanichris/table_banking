8f3eed8e2e53dc8a0358a27c64bd2c371a264773
hanichris <hanichris71@gmail.com>
2023-08-31 21:52:39 +0000

    Make api calls upon form submission to the server backend Refactor module to utilise a spacing of 2

--- app/frontend/src/components/modals/signin.tsx
+++ app/frontend/src/components/modals/signin.tsx
@@ -1,114 +1,134 @@
+import { api } from "../../api";
+import { IToken, IUserProfile } from "../../interfaces";
+
 import React, { useState } from "react";
 import { RiCloseLine } from "react-icons/ri";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import '../modal.css'
 import './form.modal.css'
+import { getLocalToken, setLocalToken } from "../../utils";
 
 interface UserFormProps {
-    isOpen: boolean,
-    setIsOpen: (param: boolean) => void,
-    setStatus: (param: string) => void,
+  isOpen: boolean,
+  setIsOpen: (param: boolean) => void,
+  setStatus: (param: string) => void,
 }
 
 
 export default function SignIn({ isOpen, setIsOpen, setStatus }: UserFormProps) {
-    const [pwdType, setPwdType] = useState('password');
-    const [pwdInput, setPwdInput] = useState('');
+  const [pwdType, setPwdType] = useState('password');
+  const [pwdInput, setPwdInput] = useState('');
 
-    function handlePwdChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setPwdInput(e.target.value);
+  function handlePwdChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPwdInput(e.target.value);
+  }
+
+  function togglePwdType() {
+    pwdType === 'password' ? setPwdType('text') : setPwdType('password');
+  }
+
+  async function handleFormSubmit(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    let responseJson: IToken;
+    let users: IUserProfile;
+
+    try {
+      const resp = await api.loginGetToken(
+        formData.get('username') as string,
+        formData.get('password') as string,
+      );
+      if (!resp.ok) {
+        throw new Error("Network response was not OK");  
+      }
+      responseJson = await resp.json();
+      setLocalToken(responseJson.access_token);
+      const resp2 = await api.getUsers(getLocalToken() as string);
+      if (!resp2.ok) {
+        throw new Error("Network response was not OK");
+      }
+      users = await resp2.json();
+      console.log(users);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        console.error(`Unexpected error: ${err}`);
+      }
     }
-
-    function togglePwdType() {
-        pwdType === 'password' ? setPwdType('text') : setPwdType('password');
-    }
-
-    function handleFormSubmit(e: React.FormEvent<HTMLFormElement>){
-        e.preventDefault();
-        const url = new URL('http://localhost:8000/api/v1/login/access-token');
-        const data = new URLSearchParams();
-        const formData = new FormData(e.currentTarget as HTMLFormElement);
-
-        formData.forEach((value, key: string) => data.set(key, value as string));
-        fetch(url, {
-            method: 'POST',
-            body: data
-        }).then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log(error));
-    }
+  }
 
     // eslint-disable-next-line no-useless-escape
     // const regex = "[\\w!#$%&'*+\\/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+\\/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}";
-    return (
-        <section className="modal" style={isOpen ? {display: "block"} : {display: 'none'}}>
-                <div className="modal-container">
-                    <div className="modal-cell">
-                        <div className="modal-element" style={isOpen ? {position:'relative', transform: 'translate3d(0, 0, 0) scale(1)', opacity:'1'}: {}}>
-                            <header className="modal-title">
-                                <h2>Sign in</h2>
-                                <div>Sign in with your email here</div>
-                            </header>
-                            <a className="modal_button-left btn btn--icon btn--xs btn--transparent" onClick={() => setIsOpen(false)}>
-                                <RiCloseLine />
-                            </a>
-                            <div className="modal-body">
-                                <form action="" method="post" onSubmit={handleFormSubmit}>
-                                    <fieldset>
-                                        <legend className="visually-hidden">Your personal information</legend>
-                                        <div className="form-group">
-                                            <label htmlFor="your-email">Email</label>
-                                            <div className="form-field">
-                                                <span className="form-field-container">
-                                                    <input 
-                                                    type="email"
-                                                    name="username"
-                                                    id="your-email"
-                                                    placeholder="john@email.com"
-                                                    pattern="[\w!#$%&'*+\/=?`\{\|\}~^\-]+(?:\.[\w!#$%&'*+\/=?`\{\|\}~^\-])*@(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}"
-                                                    maxLength={55}
-                                                    autoComplete="on"
-                                                    accessKey="e"
-                                                    required/>
-                                                </span>
-                                            </div>
+  return (
+      <section className="modal" style={isOpen ? {display: "block"} : {display: 'none'}}>
+            <div className="modal-container">
+                <div className="modal-cell">
+                    <div className="modal-element" style={isOpen ? {position:'relative', transform: 'translate3d(0, 0, 0) scale(1)', opacity:'1'}: {}}>
+                        <header className="modal-title">
+                            <h2>Sign in</h2>
+                            <div>Sign in with your email here</div>
+                        </header>
+                        <a className="modal_button-left btn btn--icon btn--xs btn--transparent" onClick={() => setIsOpen(false)}>
+                            <RiCloseLine />
+                        </a>
+                        <div className="modal-body">
+                            <form action="" method="post" onSubmit={handleFormSubmit}>
+                                <fieldset>
+                                    <legend className="visually-hidden">Your personal information</legend>
+                                    <div className="form-group">
+                                        <label htmlFor="your-email">Email</label>
+                                        <div className="form-field">
+                                            <span className="form-field-container">
+                                                <input 
+                                                type="email"
+                                                name="username"
+                                                id="your-email"
+                                                placeholder="john@email.com"
+                                                pattern="[\w!#$%&'*+\/=?`\{\|\}~^\-]+(?:\.[\w!#$%&'*+\/=?`\{\|\}~^\-])*@(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}"
+                                                maxLength={55}
+                                                autoComplete="on"
+                                                accessKey="e"
+                                                required/>
+                                            </span>
                                         </div>
-                                        <div className="form-group form-group-size-m">
-                                            <label htmlFor="your-password">Password</label>
-                                            <div className="form-field">
-                                                <span className="form-field-container">
-                                                    <input
-                                                    type={pwdType}
-                                                    name="password"
-                                                    value={pwdInput}
-                                                    onChange={handlePwdChange}
-                                                    id="your-password"
-                                                    placeholder="password" 
-                                                    minLength={8}
-                                                    required/>
-                                                    <a className="pwd-icon" onClick={togglePwdType}>
-                                                        {pwdType === 'password' ? <VscEye /> : <VscEyeClosed />}
-                                                    </a>
-                                                </span>
-                                            </div>
+                                    </div>
+                                    <div className="form-group form-group-size-m">
+                                        <label htmlFor="your-password">Password</label>
+                                        <div className="form-field">
+                                            <span className="form-field-container">
+                                                <input
+                                                type={pwdType}
+                                                name="password"
+                                                value={pwdInput}
+                                                onChange={handlePwdChange}
+                                                id="your-password"
+                                                placeholder="password" 
+                                                minLength={8}
+                                                required/>
+                                                <a className="pwd-icon" onClick={togglePwdType}>
+                                                    {pwdType === 'password' ? <VscEye /> : <VscEyeClosed />}
+                                                </a>
+                                            </span>
                                         </div>
-                                        <button type="submit" className="btn btn--m btn--primary">Sign in</button>
-                                    </fieldset>
-                                </form>
-                                <div className="small-text _m-t-3">
-                                    By continuing you agree to our <a className="link link--black">Terms of Service</a>.
-                                    <br />
-                                    Read our <a className="link link--black">Privacy policy</a>.
-                                </div>
-                            </div>
-                            <footer className="modal-footer">
-                                Forgot password? <a className="link link--primary">Reset</a>
+                                    </div>
+                                    <button type="submit" className="btn btn--m btn--primary">Sign in</button>
+                                </fieldset>
+                            </form>
+                            <div className="small-text _m-t-3">
+                                By continuing you agree to our <a className="link link--black">Terms of Service</a>.
                                 <br />
-                                Don't have an account? <a className="link link--primary" onClick={() => setStatus('signUp')}>Sign up</a>
-                            </footer>
+                                Read our <a className="link link--black">Privacy policy</a>.
+                            </div>
                         </div>
+                        <footer className="modal-footer">
+                            Forgot password? <a className="link link--primary">Reset</a>
+                            <br />
+                            Don't have an account? <a className="link link--primary" onClick={() => setStatus('signUp')}>Sign up</a>
+                        </footer>
                     </div>
                 </div>
-            </section>
-    );
+            </div>
+        </section>
+  );
 }
