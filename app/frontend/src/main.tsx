6fd47b12e5055c5d26fef1d93a23e263a7f9e71c
hanichris <hanichris71@gmail.com>
2023-09-04 18:34:12 +0000

    Wrap the application in a context provider

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { UsersProvider } from './context/userContext.tsx'
 
 // Perform / trigger the inital render.
 // It targets the target DOM node referenced by the id `root`.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UsersProvider>
+      <App />
+    </UsersProvider>
   </React.StrictMode>,
 )
