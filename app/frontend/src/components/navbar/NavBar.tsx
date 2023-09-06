4cd578178d36c6c3e054eef702576053cebda479
hanichris <hanichris71@gmail.com>
2023-09-06 22:37:25 +0000

    Reposition `motion.nav` element to allow for dynamic sizing of sidebar.

--- app/frontend/src/components/navbar/NavBar.tsx
+++ app/frontend/src/components/navbar/NavBar.tsx
@@ -1,23 +1,43 @@
 // import { Link } from 'react-router-dom';
 import { motion, useCycle } from 'framer-motion';
+import { useRef } from 'react';
+
 import MenuToggle from './MenuToggle';
 import './navbar.css'
 import logo from '../../assets/table-bank-transparent-background.svg';
+import { useDimensions } from './useDimensions';
+
+const sideBar = {
+  open: (height: number) => ({
+    clipPath:  `circle(${height * 2 + 20}rem at 3.3rem 3rem)`,
+    transition: {
+      restDelta: 2,
+      stiffness: 20,
+      type: 'spring'
+    }
+  }),
+  closed: {
+    clipPath: 'circle(2.5rem at 3.3rem 3rem)',
+    transition: {
+      damping: 40,
+      delay: 0.1,
+      stiffness: 400,
+      type: 'spring',
+    }
+  },
+};
 
 function NavBar() {
   const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
+
   return (
     <>
       <header id='mobile-header'>
-          <motion.nav className='header_menu-btn'
-            initial={false}
-            animate={isOpen ? 'open' : 'closed'}
-          >
-            <MenuToggle toggle={() => toggleOpen()}/>
-          </motion.nav>
-          <a href="/" id='mobile_header-logo'>
-            <img src={logo} alt="logo" width={90} height={60}/>
-          </a>
+        <a href="/" id='mobile_header-logo'>
+          <img src={logo} alt="logo" width={90} height={60}/>
+        </a>
       </header>
       <header id='header'>
         <a href="/" id='header-logo'>
@@ -37,6 +57,15 @@ function NavBar() {
           </ul>
         </nav>
       </header>
+      <motion.nav className='header_menu-btn'
+        initial={false}
+        animate={isOpen ? 'open' : 'closed'}
+        ref={containerRef}
+        >
+          <div id='mobile_menu-overlay' style={isOpen ? {opacity: 1, visibility: 'visible'} : {}}></div>
+          <motion.div className='background' custom={height} variants={sideBar} />
+          <MenuToggle toggle={() => toggleOpen()}/>
+      </motion.nav>
     </>
   );
 }
