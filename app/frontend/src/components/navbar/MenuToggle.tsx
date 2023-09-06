dc54695279f63b6c6695daeb81b2beb3dd5d71b7
hanichris <hanichris71@gmail.com>
2023-09-06 22:33:13 +0000

    Refactor prop types to not use reference object

--- app/frontend/src/components/navbar/MenuToggle.tsx
+++ app/frontend/src/components/navbar/MenuToggle.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
-type PathProps = React.ComponentPropsWithRef<typeof motion.path>
+type PathProps = React.ComponentPropsWithoutRef<typeof motion.path>
 interface ToggleProp {
   toggle: () => void,
 }
