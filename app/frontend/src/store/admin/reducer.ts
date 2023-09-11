17892ef717df27f4daf8f4b6f10cd8c7d932f3a7
hanichris <hanichris71@gmail.com>
2023-09-11 21:56:27 +0000

    Define reducer to be used by `admin.`

---
+++ app/frontend/src/store/admin/reducer.ts
@@ -0,0 +1,12 @@
+import { AdminAction } from "./state";
+
+export default function adminReducer(state, action: AdminAction) {
+  switch (action.type) {
+    case 'getUsers': {
+      return 
+    }
+  
+    default:
+      break;
+  }
+}
