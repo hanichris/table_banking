3137695324478d8454579186ba677724d7ce4a63
hanichris <hanichris71@gmail.com>
2023-09-11 21:55:23 +0000

    Update interface syntax. Provide additional interface definitions.

--- app/frontend/src/interfaces/index.ts
+++ app/frontend/src/interfaces/index.ts
@@ -1,38 +1,54 @@
 import React from "react";
 
 export interface IToken {
-  access_token: string,
-  token_type:string,
+  access_token: string;
+  token_type:string;
 }
 
 export interface IUserProfile {
-  email: string,
-  is_active: boolean,
-  is_superuser: boolean,
-  full_name: string,
-  id: number,
-  banks?: Array<object>,
-  banks_admin?: Array<object>,
+  email: string;
+  is_active: boolean;
+  is_superuser: boolean;
+  full_name: string;
+  id: number;
+  banks?: Array<object>;
+  banks_admin?: Array<object>;
+}
+
+export interface IUserProfileCreate {
+  email: string;
+  full_name?: string;
+  password: string;
+  is_active?: boolean;
+  is_superuser?: boolean;
+}
+
+export interface IUserProfileUpdate {
+  email?: string;
+  full_name?: string;
+  password?: string;
+  is_active?: boolean;
+  is_superuser?: boolean;
 }
 
 export interface ContextProp {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }
 
 type State = {
-  displayForm: boolean,
-  status: string,
+  displayForm: boolean;
+  status: string;
 };
 
 export interface ModalProp {
-  state: State,
-  openForm: (paramA?: string, paramB?: boolean) => void,
+  state: State;
+  openForm: (paramA?: string, paramB?: boolean) => void;
 }
 
 export interface MenuProp extends ModalProp {
-  isOpen: boolean,
+  isOpen: boolean;
 }
 
 export interface formDataType {
-  [key: string]: FormDataEntryValue
+  [key: string]: FormDataEntryValue;
 }
