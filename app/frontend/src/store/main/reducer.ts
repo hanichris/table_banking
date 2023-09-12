7cb45121e631bce56e1a3c3bd92aab7a998cdd6f
hanichris <hanichris71@gmail.com>
2023-09-12 21:24:39 +0000

    Update the main reducer to utilise the reduxjs/toolkit createSlice function.

--- app/frontend/src/store/main/reducer.ts
+++ app/frontend/src/store/main/reducer.ts
@@ -1,43 +1,65 @@
-import { IUserProfile } from "../../interfaces";
-import { Action, MainState } from "./state";
+import { createSlice } from "@reduxjs/toolkit";
 
-export default function usersReducer(state: MainState, action: Action) {
-  switch (action.type) {
-    case 'main/loggedIn': {
-      return {
-        ...state,
-        logInError: action.payload.logInError,
-        isLoggedIn: action.payload.isLoggedIn,
-        token: action.payload.token,
-        userProfile: action.payload.userProfile as IUserProfile,
-      };
-    }
+// import { IUserProfile } from "../../interfaces";
+// import { MainState } from "./state";
+// import type { RootState } from "../store";
 
-    case 'main/logInError': {
-      return {
-        ...state,
-        logInError: action.payload.logInError,
-        isLoggedIn: action.payload.isLoggedIn,
-        token: action.payload.token,
-        userProfile: action.payload.userProfile,
-      }
-    }
-    case 'main/loggedOut': {
-      return {
-        ...state,
-        isLoggedIn: action.payload.isLoggedIn,
-        token: action.payload.token,
-      };
-    }
-
-    case 'main/getMe': {
-      return {
-        ...state,
-        userProfile: action.payload.userProfile
-      }
-    }
-
-    default:
-      throw new Error(`Unrecognized action: ${action.type}`);
+export const mainSlice = createSlice({
+  name: 'main',
+  initialState: undefined,
+  reducers: {
+    loggedIn: () => {
+      console.log('Not implemented');
+    },
+    logInError: () => {
+      console.log('Not implemented');
+    },
+    loggedOut: () => {
+      console.log('Not implemented');
+    },
   }
-}
+});
+
+export const { loggedIn, logInError, loggedOut } = mainSlice.actions;
+export default mainSlice.reducer;
+
+// export default function usersReducer(state: MainState, action: Action) {
+//   switch (action.type) {
+//     case 'main/loggedIn': {
+//       return {
+//         ...state,
+//         logInError: action.payload.logInError,
+//         isLoggedIn: action.payload.isLoggedIn,
+//         token: action.payload.token,
+//         userProfile: action.payload.userProfile as IUserProfile,
+//       };
+//     }
+
+//     case 'main/logInError': {
+//       return {
+//         ...state,
+//         logInError: action.payload.logInError,
+//         isLoggedIn: action.payload.isLoggedIn,
+//         token: action.payload.token,
+//         userProfile: action.payload.userProfile,
+//       }
+//     }
+//     case 'main/loggedOut': {
+//       return {
+//         ...state,
+//         isLoggedIn: action.payload.isLoggedIn,
+//         token: action.payload.token,
+//       };
+//     }
+
+//     case 'main/getMe': {
+//       return {
+//         ...state,
+//         userProfile: action.payload.userProfile
+//       }
+//     }
+
+//     default:
+//       throw new Error(`Unrecognized action: ${action.type}`);
+//   }
+// }
