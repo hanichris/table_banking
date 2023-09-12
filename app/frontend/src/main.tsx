d15bd32729992b9d50943d53a27d030bdecac1a3
hanichris <hanichris71@gmail.com>
2023-09-12 20:37:56 +0000

    Migrate from `context Provider` to `Provider` component in react-redux

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import { store } from './store/store.ts'
 import './index.css'
 import App from './App.tsx'
-import { UsersProvider } from './context/userContext.tsx'
+// import { UsersProvider } from './context/userContext.tsx'
 
 // Perform / trigger the inital render.
 // It targets the target DOM node referenced by the id `root`.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UsersProvider>
+    <Provider store={store}>
       <App />
-    </UsersProvider>
+    </Provider>
   </React.StrictMode>,
 )
