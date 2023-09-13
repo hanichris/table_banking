50fb995498922b4e188e80f09937825fe21bf118
hanichris <hanichris71@gmail.com>
2023-09-13 20:50:58 +0000

    Define the skeleton for the bank reducer slice of the redux store.

---
+++ app/frontend/src/store/bank/reducer.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const bankSlice = createSlice({
+  name: 'bank',
+  initialState,
+  reducers: {}
+});
+
+export default bankSlice.reducer;
