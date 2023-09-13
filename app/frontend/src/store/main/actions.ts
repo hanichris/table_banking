bf6785d5e88e43e4fd9ee18b6ebc9285d5cf2817
hanichris <hanichris71@gmail.com>
2023-09-13 20:52:51 +0000

    Re-implement the actions object for the action creators for the main slice of the redux store. Employs `createAsyncThunk` API to enable async logic.

--- app/frontend/src/store/main/actions.ts
+++ app/frontend/src/store/main/actions.ts
@@ -1,64 +1,99 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
 import { api } from "../../api";
 import { IToken, IUserProfile } from "../../interfaces";
-import { removeLocalToken, setLocalToken } from "../../utils";
+import { setLocalToken } from "../../utils";
+
+type LogInResponse = {
+  token: string,
+  isLoggedIn: boolean,
+  userProfile: IUserProfile | null,
+}
 
 export const actions = {
   actionGetUserProfile:async (token:string) => {
-    let respJson: IUserProfile | undefined = undefined;
     const resp = await api.getMe(token);
     if (!resp.ok) {
       throw new Error("Network response was not OK!!!");
     }
-    respJson = await resp.json();
+    const respJson = await resp.json();
     return respJson;
   },
-  actionLogIn:async (payload:{username: string, password:string}) => {
-    let respJson: IToken;
-    try {
-      const resp = await api.loginGetToken(payload.username, payload.password);
-      if (!resp.ok) {
-        throw new Error("Network response was not OK");
-      }
-      respJson = await resp.json();
-      const token = respJson.access_token;
-      if (token) {
-        setLocalToken(token);
-        const userProfile = await actions.actionGetUserProfile(token);
-        return {
-          type: 'main/loggedIn',
-          payload: {
-            logInError: false,
-            isLoggedIn: true,
-            token,
-            userProfile,
-          }
-        };
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        console.error(`Unexpected error: ${err}`);
-      }
-      }
-    return {
-      type: 'main/logInError',
-      payload: {
-        logInError: true,
-        isLoggedIn: false,
-        token: '',
-        userProfile: undefined,
-      }
+  logIn: createAsyncThunk('main/logIn',async (payload:{uname: string, pwd: string}) => {
+    const result: LogInResponse = {token: '', isLoggedIn: false, userProfile: null};
+    const resp = await api.loginGetToken(payload.uname, payload.pwd);
+    if (!resp.ok) {
+      throw new Error("Network response was not OK!!!");
     }
-  },
-  actionLogOut:async () => {
-    removeLocalToken();
-    return {
-      type: 'main/loggedOut',
-      payload: {
-        isLoggedIn: false,
-        token: '',
-      }
-    };
-  },
+    const respJson: IToken = await resp.json();
+    const token = respJson.access_token;
+    if (token) {
+      setLocalToken(token);
+      result.token = token;
+      result.isLoggedIn = true;
+      result.userProfile = await actions.actionGetUserProfile(token);
+    }
+    return result;
+  }),
 };
+
+// export const actions = {
+//   actionGetUserProfile:async (token:string) => {
+//     let respJson: IUserProfile | undefined = undefined;
+//     const resp = await api.getMe(token);
+//     if (!resp.ok) {
+//       throw new Error("Network response was not OK!!!");
+//     }
+//     respJson = await resp.json();
+//     return respJson;
+//   },
+//   actionLogIn:async (payload:{username: string, password:string}) => {
+//     let respJson: IToken;
+//     try {
+//       const resp = await api.loginGetToken(payload.username, payload.password);
+//       if (!resp.ok) {
+//         throw new Error("Network response was not OK");
+//       }
+//       respJson = await resp.json();
+//       const token = respJson.access_token;
+//       if (token) {
+//         setLocalToken(token);
+//         const userProfile = await actions.actionGetUserProfile(token);
+//         return {
+//           type: 'main/loggedIn',
+//           payload: {
+//             logInError: false,
+//             isLoggedIn: true,
+//             token,
+//             userProfile,
+//           }
+//         };
+//       }
+//     } catch (err) {
+//       if (err instanceof Error) {
+//         console.error(err.message);
+//       } else {
+//         console.error(`Unexpected error: ${err}`);
+//       }
+//       }
+//     return {
+//       type: 'main/logInError',
+//       payload: {
+//         logInError: true,
+//         isLoggedIn: false,
+//         token: '',
+//         userProfile: undefined,
+//       }
+//     }
+//   },
+//   actionLogOut:async () => {
+//     removeLocalToken();
+//     return {
+//       type: 'main/loggedOut',
+//       payload: {
+//         isLoggedIn: false,
+//         token: '',
+//       }
+//     };
+//   },
+// };
