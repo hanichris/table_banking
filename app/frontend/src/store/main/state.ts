0c35bdffd542312bd18baa7f56b493ccd22c1773
hanichris <hanichris71@gmail.com>
2023-09-13 20:48:04 +0000

    Define the state interface for the main slice of the store.

--- app/frontend/src/store/main/state.ts
+++ app/frontend/src/store/main/state.ts
@@ -2,13 +2,14 @@ import { IUserProfile } from "../../interfaces";
 
 
 export interface MainState {
-    logInError: boolean,
-    isLoggedIn: boolean,
-    token: string,
-    userProfile?: IUserProfile,
+    error: string | undefined; // 'logInError', 'signUpError', etc.
+    isLoggedIn: boolean | null;
+    status: 'idle' | 'loading' | 'succeeded' | 'failed'; // API call state.
+    token: string;
+    userProfile: IUserProfile | null;
 }
 
 export interface Action {
-    type: string,
-    payload: MainState,
+    type: string;
+    payload: MainState;
 }
