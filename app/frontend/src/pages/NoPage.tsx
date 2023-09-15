e63e3e2d9555510b09ab3aee990fecffed2d5b47
hanichris <hanichris71@gmail.com>
2023-09-15 22:25:15 +0000

    Create a rudimentary `NoPage` found component. Serves as the error element for the router component when accessing a link that doesn't lead anywhere.

--- app/frontend/src/pages/NoPage.tsx
+++ app/frontend/src/pages/NoPage.tsx
@@ -1,5 +1,28 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 export default function NoPage() {
-    return (
-        <></>
-    );
+  // 
+  const error = useRouteError();
+  const message = errorMessage(error);
+  return (
+    <div id="error-page">
+      <h1>Oops!!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{message}</i>
+      </p>
+    </div>
+  );
 }
+
+const errorMessage = (err: unknown):string => {
+  if (isRouteErrorResponse(err)) {
+    return `${err.status} ${err.statusText}`;
+  } else if (err instanceof Error) {
+    return err.message;
+  } else if (typeof err === 'string') {
+    return err;
+  }
+  console.error(err);
+  return 'Unknown error';
+};
