f08c2d7bd0069425440fb41c3087529f65b111a0
hanichris <hanichris71@gmail.com>
2023-09-15 22:23:34 +0000

    Create an account page component. Serves to showcase a user's account page.

---
+++ app/frontend/src/pages/Account.tsx
@@ -0,0 +1,17 @@
+import './account.css'
+
+export default function Account() {
+  return (
+    <div className="wrapper wrapper--align-top">
+      <div>
+        <div className='container container--s'>
+          <section className='page-title'>
+            <h1 className='page-title__header'>Account</h1>
+            <p className='page_title__subheader'>Update your profile and set your account preferences</p>
+          </section>
+        </div>
+        <div className='container container--xl'></div>
+      </div>
+    </div>
+  );
+}
