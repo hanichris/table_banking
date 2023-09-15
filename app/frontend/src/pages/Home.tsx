83e2538fde88e560d540c7e5fcfec67e1bb083af
hanichris <hanichris71@gmail.com>
2023-09-15 22:24:19 +0000

    Holds the navbar component.

--- app/frontend/src/pages/Home.tsx
+++ app/frontend/src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import NavBar from "../components/navbar/NavBar";
+import Account from "./Account";
+// import UserProfile from "./UserProfile";
+
 export default function Home() {
-    return (
-        <></>
-    );
+  return (
+    <>
+      <NavBar/>
+      <Account />
+    </>
+  );
 }
