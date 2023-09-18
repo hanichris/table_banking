102eeead999cd3513966eaad1fdbf56b2bb03242
hanichris <hanichris71@gmail.com>
2023-09-18 00:01:02 +0000

    Change the default state interface definition to `string | null` from `string | undefined`.

--- app/frontend/src/store/main/state.ts
+++ app/frontend/src/store/main/state.ts
@@ -2,7 +2,7 @@ import { IUserProfile } from "../../interfaces";
 
 
 export interface MainState {
-    error: string | undefined; // 'logInError', 'signUpError', etc.
+    error: string | null; // 'logInError', 'signUpError', etc.
     isLoggedIn: boolean | null;
     status: 'idle' | 'loading' | 'succeeded' | 'failed'; // API call state.
     token: string;
