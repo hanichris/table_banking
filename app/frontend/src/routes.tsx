2a02ed8d763312eed11aa3a632c03fec12641884
hanichris <hanichris71@gmail.com>
2023-09-26 22:29:47 +0000

    Make the landing page the default page. Examine ability to change to a different route.

--- app/frontend/src/routes.tsx
+++ app/frontend/src/routes.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import NoPage from "./pages/NoPage";
 import Layout from "./pages/Layout";
+import HomePage from "./pages/Home";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: '',
     element: <Layout />,
     errorElement: <NoPage />,
     children: [
       {
-        path: '',
-        element: null,
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: 'dashboard',
+        element: <h1>Welcome to the dashboard</h1>,
       }
     ],
   }
