3599d43e4f51996df892de82e2443f107d2afd89
hanichris <hanichris71@gmail.com>
2023-09-26 22:00:28 +0000

    Create a landing page component.

--- app/frontend/src/pages/Home.tsx
+++ app/frontend/src/pages/Home.tsx
@@ -1,12 +1,18 @@
-import NavBar from "../components/navbar/NavBar";
-import Account from "./Account";
-// import UserProfile from "./UserProfile";
+import { CiBank } from 'react-icons/ci';
 
-export default function Home() {
+import './home.css';
+
+export default function HomePage() {
   return (
-    <>
-      <NavBar/>
-      <Account />
-    </>
+    <div className='wrapper page-wrapper'>
+      <div className='container'>
+        <section className='landing-page'>
+          <div>
+            <CiBank className='bank-logo'/>
+          </div>
+          <h2> Welcome to your future of table banking</h2>
+        </section>
+      </div>
+    </div>
   );
 }
