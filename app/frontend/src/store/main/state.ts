81646cab1ed0b1315bbe6832709c0967af075f01
hanichris <hanichris71@gmail.com>
2023-09-26 22:03:14 +0000

    Change the interface definition to use entites instead of a userProfile entry. Adds support for normalized state.

--- app/frontend/src/store/main/state.ts
+++ app/frontend/src/store/main/state.ts
@@ -1,12 +1,9 @@
-import { IUserProfile } from "../../interfaces";
-
-
 export interface MainState {
     error: string | null; // 'logInError', 'signUpError', etc.
     isLoggedIn: boolean | null;
     status: 'idle' | 'loading' | 'succeeded' | 'failed'; // API call state.
     token: string;
-    userProfile: IUserProfile | null;
+    entities: object;
 }
 
 export interface Action {
