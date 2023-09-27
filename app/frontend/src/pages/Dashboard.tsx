1a338702b39687ff499b1f8784fc6efd0bc747eb
hanichris <hanichris71@gmail.com>
2023-09-27 22:39:52 +0000

    Implementation of the dashboard page.

---
+++ app/frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from 'react-router-dom';
+
+import './dashboard.css'
+
+
+export default function Dashboard() {
+  return (
+    <section className='dashboard'>
+      <aside className='dashboard--sidebar'>
+        <nav className='dashboard--sidebar_nav'>
+          <ul>
+            <li><Link to='/dashboard/banks'>Banks</Link></li>
+            <li><Link to='/dashboard/account'>Account</Link></li>
+          </ul>
+        </nav>
+      </aside>
+      <main className='dashboard_content'>
+        <Outlet />
+      </main>
+    </section>
+  );
+}
