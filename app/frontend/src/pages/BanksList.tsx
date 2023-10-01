51ae346a91efdaac66efabb45f03de80ae6c0d16
hanichris <hanichris71@gmail.com>
2023-10-01 22:46:55 +0000

    Skeleton set up for the bank listing page.

---
+++ app/frontend/src/pages/BanksList.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function BanksList() {
+
+  return (
+    <>
+      <h2>Banks Listing page</h2>
+      <Link to='1'>First Bank</Link>
+    </>
+  );
+}
