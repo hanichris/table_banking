6ae3a4c52ae2e4abeacfff2be90db97ad12e5db7
hanichris <hanichris71@gmail.com>
2023-10-03 19:39:07 +0000

    Include icon to serve as a `back` indicator.

--- app/frontend/src/pages/Bank.tsx
+++ app/frontend/src/pages/Bank.tsx
@@ -1,12 +1,17 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { IoChevronBack } from 'react-icons/io5';
 
 export default function Bank() {
   const navigate = useNavigate();
   const params = useParams();
   return (
-    <>
-      <a onClick={() => navigate(-1)}>Back</a>
-      <h2>Bank {params.bankId} page</h2>
-    </>
+    <div id="bank-page">
+      <a onClick={() => navigate(-1)}><IoChevronBack /></a>
+      <header className="bank-page__top">
+        <h2 className="bank-page__title">
+          Bank {params.bankId} page
+        </h2>
+      </header>
+    </div>
   )
 }
