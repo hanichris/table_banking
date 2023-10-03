1320f21e326ac05b1fcff7486c64535f310337de
hanichris <hanichris71@gmail.com>
2023-10-03 19:40:26 +0000

    Use `Navlink` to dynamically add `active` class to an anchor tag.

--- app/frontend/src/pages/Dashboard.tsx
+++ app/frontend/src/pages/Dashboard.tsx
@@ -1,17 +1,39 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLoaderData } from 'react-router-dom';
+import { AiOutlineBank, AiOutlineProfile, AiOutlineUser } from 'react-icons/ai'
 
 import './dashboard.css'
+import { MainState } from '../store/main/state';
 
 
 export default function Dashboard() {
+  const user = useLoaderData() as MainState;
+
+  user.entities
+
+
   return (
     <section className='dashboard'>
       <aside className='dashboard--sidebar'>
         <nav className='dashboard--sidebar_nav'>
-          <ul>
-            <li><Link to='/dashboard/banks'>Banks</Link></li>
-            <li><Link to='/dashboard/account'>Account</Link></li>
-            <li><Link to='/dashboard/profile'>Profile</Link></li>
+          <ul className='dashboard--sidebar_nav_group'>
+            <li>
+              <NavLink to='/dashboard/banks'>
+                <AiOutlineBank className='icon--dashboard'/>
+                <div data-label='Banks'></div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/dashboard/account'>
+                <AiOutlineUser className='icon--dashboard'/>
+                <div data-label='Account'></div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/dashboard/profile'>
+                <AiOutlineProfile className='icon--dashboard'/>
+                <div data-label='Profile'></div>
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </aside>
