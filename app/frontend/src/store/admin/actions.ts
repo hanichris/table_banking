bab73d5c0b2315205ff8381b0c3146e1aa0398bf
hanichris <hanichris71@gmail.com>
2023-10-03 19:43:22 +0000

    refactor the actions file for the admin store.

--- app/frontend/src/store/admin/actions.ts
+++ app/frontend/src/store/admin/actions.ts
@@ -1,23 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
 import { api } from "../../api";
-import { MainState } from "../main/state";
+// import { MainState } from "../main/state";
 
 export const actions = {
-  actionCreateUser: '',
-  actionGetUsers:async (context: MainState) => {
-    try {
-      const resp = await api.getUsers(context.token);
-      if (!resp.ok) {
-        throw new Error("Network response was not OK!!!");
-      }
-      
-    } catch (err) {
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        console.error(`Unexpected error: ${err}`);
-      }
+  createUser: '',
+  getUsers: createAsyncThunk('admin/getUsers',async (token: string) => {
+    const resp = await api.getUsers(token);
+    if (!resp.ok) {
+      throw new Error("Network response was not OK!!!");
     }
-    return {};
-  },
-  actionUpdateUser: '',
+  }),
+  updateUser: '',
+  deleteUser: '',
 };
