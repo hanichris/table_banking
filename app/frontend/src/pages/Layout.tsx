ac4348cfd0481e27225cafe35628222741901fb3
hanichris <hanichris71@gmail.com>
2023-10-03 19:41:14 +0000

    Redirect a user to the dashboard if they were already logged in.

--- app/frontend/src/pages/Layout.tsx
+++ app/frontend/src/pages/Layout.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigate } from "react-router-dom";
 
 import NavBar from "../components/navbar/NavBar";
+import { useLayoutEffect } from "react";
+import { MainState } from "../store/main/state";
 
 export default function Layout() {
+  const user = useLoaderData() as MainState | null;
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    let loggedIn = false;
+    if (user && user.isLoggedIn == true) {
+      loggedIn = true;
+      const goToDashboard = () => navigate('/dashboard');
+      if (loggedIn) goToDashboard();
+    }
+    return () => { loggedIn = false; }
+  }, [user, navigate])
+
   return (
     <>
       <NavBar />
