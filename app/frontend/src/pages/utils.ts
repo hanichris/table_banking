37049794775865f0050175a7c60115d0dc9252fb
hanichris <hanichris71@gmail.com>
2023-10-03 19:42:12 +0000

    Implement the loader function for the root route. If a user has a token saved in their browser's localStorage, then try and log them in to the app.

--- app/frontend/src/pages/utils.ts
+++ app/frontend/src/pages/utils.ts
@@ -1,6 +1,9 @@
 import { redirect } from 'react-router-dom';
 import { store } from '../store';
 import { SelectMain } from '../store/main/selectors';
+import { getLocalToken, removeLocalToken } from '../utils';
+import { actions } from '../store/main/actions';
+import { getMe } from '../store/main/reducer';
 
 
 export async function dashboardLoader() {
@@ -9,4 +12,19 @@ export async function dashboardLoader() {
     throw redirect('/');
   }
   return user;
+}
+
+export async function layoutLoader() {
+  const token = getLocalToken() as string | null;
+  if (token) {
+    try {
+      const userProfile = await actions.getMe(token);
+      store.dispatch(getMe({userProfile, token}));
+      return SelectMain(store.getState());
+    } catch (error) {
+      removeLocalToken();
+      return null;
+    }
+  }
+  return null;
 }
