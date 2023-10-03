1882fd19ad5c2f77c93892f1642edb141ba9133b
hanichris <hanichris71@gmail.com>
2023-10-03 19:39:26 +0000

    Flesh out the BanksLists component.

--- app/frontend/src/pages/BanksList.tsx
+++ app/frontend/src/pages/BanksList.tsx
@@ -3,9 +3,15 @@ import { Link } from "react-router-dom";
 export default function BanksList() {
 
   return (
-    <>
-      <h2>Banks Listing page</h2>
-      <Link to='1'>First Bank</Link>
-    </>
+    <div id="dashboard_bank-listings">
+      <header className="bank-listings__top">
+        <h2 className="bank-listings__title">Bank Listings</h2>
+      </header>
+      <div className="bank-listings__body">
+        <div className="bank-listings_content_none">
+          <Link to='1'>First Bank</Link>
+        </div>
+      </div>
+    </div>
   );
 }
