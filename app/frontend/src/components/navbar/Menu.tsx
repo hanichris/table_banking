4cc937fcf4880a9b9e2f41f466a25d48ad613e65
hanichris <hanichris71@gmail.com>
2023-10-03 19:37:18 +0000

    Change what is rendered in the menu list depending on if a user is logged in.

--- app/frontend/src/components/navbar/Menu.tsx
+++ app/frontend/src/components/navbar/Menu.tsx
@@ -1,7 +1,11 @@
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 import { SignInSignUp } from '../modals/ModalForm';
 import { MenuProp } from '../../interfaces';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { loggedOut } from '../../store/main/reducer';
+import { removeLocalToken } from '../../utils';
 
 
 const ulVariants = {
@@ -30,7 +34,38 @@ const liVariants = {
   },
 };
 
-export const Menu = ( { state, openForm, isOpen }: MenuProp ) => {
+export const Menu = ( { state, openForm, isOpen, toggle }: MenuProp ) => {
+  const loggedIn = useAppSelector(state => state.main.isLoggedIn);
+  const dispatch = useAppDispatch();
+
+  const handleLogOut = () => {
+    removeLocalToken();
+    dispatch(loggedOut());
+  };
+
+  const listItemLoggedOut = <>
+    <motion.li variants={liVariants}>
+      <a onClick={() => openForm('signIn')}>Sign In</a>
+    </motion.li>
+    <motion.li variants={liVariants}>
+      <a onClick={() => openForm('signUp')}>Sign Up</a>
+    </motion.li>
+  </>
+
+  const listItemLoggedIn = <>
+    <motion.li variants={liVariants}>
+      <Link to='/dashboard/banks' onClick={toggle}>Banks</Link>
+    </motion.li>
+    <motion.li variants={liVariants}>
+      <Link to='/dashboard/account' onClick={toggle}>Account</Link>
+    </motion.li>
+    <motion.li variants={liVariants}>
+      <Link to='/dashboard/profile' onClick={toggle}>Profile</Link>
+    </motion.li>
+    <motion.li variants={liVariants}>
+      <Link to='/' onClick={handleLogOut}>Sign Out</Link>
+    </motion.li>
+  </>
 
   return (
     <>
@@ -38,12 +73,7 @@ export const Menu = ( { state, openForm, isOpen }: MenuProp ) => {
         <motion.li variants={liVariants}>
           <div>Account</div>
         </motion.li>
-        <motion.li variants={liVariants}>
-          <a onClick={() => openForm('signIn')}>Sign In</a>
-        </motion.li>
-        <motion.li variants={liVariants}>
-          <a onClick={() => openForm('signUp')}>Sign Up</a>
-        </motion.li>
+        {loggedIn === true ? listItemLoggedIn : listItemLoggedOut}
       </motion.ul>
       <SignInSignUp state={state} openForm={openForm}/> 
     </>
