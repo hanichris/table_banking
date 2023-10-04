da21b59455b7e49e5f8bb9abf6f7b5ac5cc3d0e9
hanichris <hanichris71@gmail.com>
2023-10-04 21:58:15 +0000

    Define the interface for the entities entry in the state.

--- app/frontend/src/store/main/state.ts
+++ app/frontend/src/store/main/state.ts
@@ -1,9 +1,21 @@
+import { IBank, IUserProfile } from "../../interfaces";
+
+interface IUser extends IUserProfile {
+    banks: Array<number>;
+    banks_admin: Array<number>;
+}
+
+type Entity = {
+    bank: Record<string, IBank>,
+    user: Record<string, IUser>,
+};
+
 export interface MainState {
     error: string | null; // 'logInError', 'signUpError', etc.
     isLoggedIn: boolean | null;
     status: 'idle' | 'loading' | 'succeeded' | 'failed'; // API call state.
     token: string;
-    entities: object;
+    entities: Entity;
 }
 
 export interface Action {
