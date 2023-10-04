9dcaa7ec78fbfa34c9018a276caf7b8da1244d37
hanichris <hanichris71@gmail.com>
2023-10-04 21:57:53 +0000

    Return a list of keys and pick out the bank data based on the provided ID.

--- app/frontend/src/store/main/selectors.ts
+++ app/frontend/src/store/main/selectors.ts
@@ -1,4 +1,11 @@
 import { RootState } from "..";
 
-export const SelectMain = (state: RootState) => state.main;
+export const selectMain = (state: RootState) => state.main;
+export const selectUserBankById = (state: RootState, bankId: string) => {
+  return state.main.entities.bank[bankId];
+};
+
+export const selectAllUserBankIds = (state: RootState)=> {
+  return Object.keys(state.main.entities.bank);
+};
 
