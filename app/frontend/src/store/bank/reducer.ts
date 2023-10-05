7d28e5de57154fd5c83ad5f5b02c71ad52959a95
hanichris <hanichris71@gmail.com>
2023-10-05 19:15:12 +0000

    Provide an extraReducer to handle the case of fetching data of a bank with a given ID.

--- app/frontend/src/store/bank/reducer.ts
+++ app/frontend/src/store/bank/reducer.ts
@@ -1,11 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { normalize, schema } from "normalizr";
 
-const initialState = {};
+import { bankDefaultState as defaultState } from ".";
+import { bankActions } from "./actions";
+
+const userEntity = new schema.Entity('user');
+const bankEntity = new schema.Entity('bank', {members: [userEntity]});
 
 export const bankSlice = createSlice({
   name: 'bank',
-  initialState,
-  reducers: {}
+  initialState: defaultState,
+  reducers: {},
+  extraReducers(builder){
+    builder
+      .addCase(bankActions.getBank.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(bankActions.getBank.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        const normalizedBank = normalize(action.payload, bankEntity);
+        const { bank, user} = normalizedBank.entities;
+        if (bank) {
+          const { members, ...rest} = bank[action.payload.id];
+          Object.assign(state, rest);
+          state.members.ids = members.slice();
+        }
+        Object.assign(state.members.entities, user);
+      })
+      .addCase(bankActions.getBank.rejected, (state, action) => {
+        Object.assign(state, defaultState);
+        state.status = 'failed';
+        state.error = action.error.message as string;
+      })
+  }
 });
 
 export default bankSlice.reducer;
