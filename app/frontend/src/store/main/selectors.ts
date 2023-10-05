00773bb48b827333d258f1cf2b1fe6424112fae7
hanichris <hanichris71@gmail.com>
2023-10-05 19:17:04 +0000

    Create a memoized selector to prevent unnecessary re-renders. Provide token selector as well.

--- app/frontend/src/store/main/selectors.ts
+++ app/frontend/src/store/main/selectors.ts
@@ -1,3 +1,5 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 import { RootState } from "..";
 
 export const selectMain = (state: RootState) => state.main;
@@ -5,7 +7,14 @@ export const selectUserBankById = (state: RootState, bankId: string) => {
   return state.main.entities.bank[bankId];
 };
 
-export const selectAllUserBankIds = (state: RootState)=> {
-  return Object.keys(state.main.entities.bank);
+export const selectUserToken = (state: RootState) => state.main.token;
+
+const selectAllUserBanks = (state: RootState)=> {
+  return state.main.entities.bank;
 };
+
+export const selectAllUserBankIds = createSelector(
+  [selectAllUserBanks],
+  (banks) => Object.keys(banks)
+);
 
