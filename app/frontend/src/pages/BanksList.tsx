908a224b04fd31c1ff52c19f2e859a5c047e1c41
hanichris <hanichris71@gmail.com>
2023-10-06 18:00:28 +0000

    Add a search btn to filter the BanksList. Add a new btn to create a new bank for the user.

--- app/frontend/src/pages/BanksList.tsx
+++ app/frontend/src/pages/BanksList.tsx
@@ -1,3 +1,6 @@
+import { Form } from "react-router-dom";
+import { FiSearch } from 'react-icons/fi';
+
 import { useAppSelector } from "../hooks";
 import { selectAllUserBankIds } from "../store/main/selectors";
 import BankExcerpt from "./BankExcerpt";
@@ -13,6 +16,26 @@ export default function BanksList() {
     <div id="dashboard_bank-listings">
       <header className="bank-listings__top">
         <h2 className="bank-listings__title">Bank Listings</h2>
+        <div className="bank-listings__filters">
+          <div className="bank-listings__search">
+            <Form id="search-form" role="search">
+              <input
+              id="q"
+              placeholder="Search name"
+              name="q"
+              type="search" />
+              <span className="input-search__search-btn">
+                <i className="icon"></i>
+              </span>
+            </Form>
+          </div>
+          <button id='mobile__search-btn' className="btn btn--s btn--icon btn--secondary">
+            <i className="icon">
+              <FiSearch />
+            </i>
+          </button>
+          <button className="btn btn--s btn--icon btn--secondary">+</button>
+        </div>
       </header>
       <div className="bank-listings__body">
         {banks.length === 0 ? <div className="bank-listings_content_none">
