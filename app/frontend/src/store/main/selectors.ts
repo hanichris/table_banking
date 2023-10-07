1f1c30987d3650ec445308dbdc041d45d8bbcea4
hanichris <hanichris71@gmail.com>
2023-10-07 23:40:42 +0000

    refactor selector functions to match state definition of the main slice.

--- app/frontend/src/store/main/selectors.ts
+++ app/frontend/src/store/main/selectors.ts
@@ -4,13 +4,13 @@ import { RootState } from "..";
 
 export const selectMain = (state: RootState) => state.main;
 export const selectUserBankById = (state: RootState, bankId: string) => {
-  return state.main.entities.bank[bankId];
+  return state.main.user.banks[bankId];
 };
 
 export const selectUserToken = (state: RootState) => state.main.token;
 
-const selectAllUserBanks = (state: RootState)=> {
-  return state.main.entities.bank;
+export const selectAllUserBanks = (state: RootState)=> {
+  return state.main.user.banks;
 };
 
 export const selectAllUserBankIds = createSelector(
