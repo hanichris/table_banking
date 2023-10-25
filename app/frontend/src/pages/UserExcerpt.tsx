c4ece08037b8f06109ffd12b8b8a5de887d6cb5a
hanichris <hanichris71@gmail.com>
2023-10-25 19:54:20 +0000

    An individual row of the grid table with a user's details. Includes `delete` and `edit` column entries.

--- app/frontend/src/pages/UserExcerpt.tsx
+++ app/frontend/src/pages/UserExcerpt.tsx
@@ -1,17 +1,24 @@
+import { FiEdit2 } from 'react-icons/fi';
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 import { IUser } from "../interfaces";
 
 export default function UserExcerpt({ user }: {user: IUser}) {
   return (
-    <li>
-      <div>
-        {user.full_name}
-      </div>
-      <div>
-        {user.full_name}
-      </div>
-      <div>
-        {user.is_active}
-      </div>
-    </li>
+    <tr>
+      <td className="id_col">{user.id}</td>
+      <td className="name_col">{user.full_name}</td>
+      <td className="email_col">{user.email}</td>
+      <td className="active_col">{user.is_active === true ? '✔' : '✘'}</td>
+      <td className="admin_col">{user.is_superuser === true ? '✔' : '✘'}</td>
+      <td className="edit_col">
+        <a>
+          <FiEdit2 style={{marginTop: '.5rem', fontSize: '1.8rem', stroke: '#08805b'}}/>
+        </a>
+      </td>
+      <td className="del_col">
+        <RiDeleteBin6Line style={{marginTop: '.5rem', fontSize: '1.8rem', fill: '#cd3e65'}}/>
+      </td>
+    </tr>
   );
 }
