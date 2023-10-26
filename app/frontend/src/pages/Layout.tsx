9e0e08b79fff361a7eb39ad79a6b25cec97727fb
hanichris <hanichris71@gmail.com>
2023-10-26 00:26:48 +0000

    fix: Clean up the Layout page. Removes navigation. Ensures the page simply renders the layout.

--- app/frontend/src/pages/Layout.tsx
+++ app/frontend/src/pages/Layout.tsx
@@ -1,23 +1,8 @@
-import { Outlet, useLoaderData, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import NavBar from "../components/navbar/NavBar";
-import { useLayoutEffect } from "react";
-import { MainState } from "../store/main/state";
 
 export default function Layout() {
-  const user = useLoaderData() as MainState | null;
-  const navigate = useNavigate();
-
-  useLayoutEffect(() => {
-    let loggedIn = false;
-    if (user && user.isLoggedIn == true) {
-      console.log('Navigating to the dashboard...')
-      loggedIn = true;
-      const goToDashboard = () => navigate('/dashboard');
-      if (loggedIn) goToDashboard();
-    }
-    return () => { loggedIn = false; }
-  }, [user, navigate])
 
   return (
     <>
