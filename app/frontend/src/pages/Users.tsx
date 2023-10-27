dce460efa611b1fcec9f0d84c8af90b8e97f228d
hanichris <hanichris71@gmail.com>
2023-10-27 18:01:42 +0000

    Utilise type guards to render appropriate component.

--- app/frontend/src/pages/Users.tsx
+++ app/frontend/src/pages/Users.tsx
@@ -3,10 +3,15 @@ import { IUser } from "../interfaces";
 import UserExcerpt from "./UserExcerpt";
 
 export default function Users() {
-  const users = useAsyncValue() as Array<IUser>;
-  return (
+  const users = useAsyncValue() as Array<IUser> | IUser;
+  if (Array.isArray(users)) {
+    return (
       users.map((user) => (
       <UserExcerpt key={user.id} user={user}/>
       ))
+    );
+  }
+  return (
+    <UserExcerpt user={users}/>
   );
 }
