b23b74d4b0fdd4b25bea496881a5c3139bb214c3
hanichris <hanichris71@gmail.com>
2023-10-27 17:58:47 +0000

    Define a Skeleton component to be rendered when displaying users in a table.

---
+++ app/frontend/src/components/skeletons/UsersSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@mui/material";
+
+export default function UsersSkeleton() {
+  return (
+    (Array.from({length: 10}, (_, idx) => ++idx).map(id => (
+      <tr key={id}>
+        <td className="skeleton-item"><Skeleton animation="pulse" sx={{fontSize: '2rem'}}/></td>
+      </tr>
+    ))
+  ));
+}
