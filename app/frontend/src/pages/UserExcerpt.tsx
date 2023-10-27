18f45990954c4612fe2a073a27ea7ce4c0b58fd6
hanichris <hanichris71@gmail.com>
2023-10-27 21:52:57 +0000

    fix: Link to edit and view routes. Launch delete modal.

--- app/frontend/src/pages/UserExcerpt.tsx
+++ app/frontend/src/pages/UserExcerpt.tsx
@@ -1,24 +1,36 @@
+import { useState } from 'react';
+
 import { FiEdit2 } from 'react-icons/fi';
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 import { IUser } from "../interfaces";
+import DeleteEntity from '../components/modals/DeleteEntityModal';
 
 export default function UserExcerpt({ user }: {user: IUser}) {
+  const [deleteBtn, setDeleteBtn] = useState(false);
   return (
     <tr>
-      <td className="id_col">{user.id}</td>
+      <td className="id_col">
+        <Link to={`${user.id}`}>
+          {user.id}
+        </Link>
+      </td>
       <td className="name_col">{user.full_name}</td>
       <td className="email_col">{user.email}</td>
       <td className="active_col">{user.is_active === true ? '✔' : '✘'}</td>
       <td className="admin_col">{user.is_superuser === true ? '✔' : '✘'}</td>
       <td className="edit_col">
-        <a>
+        <Link to={`${user.id}/edit`}>
           <FiEdit2 style={{marginTop: '.5rem', fontSize: '1.8rem', stroke: '#08805b'}}/>
-        </a>
+        </Link>
       </td>
       <td className="del_col">
-        <RiDeleteBin6Line style={{marginTop: '.5rem', fontSize: '1.8rem', fill: '#cd3e65'}}/>
+        <a onClick={() => setDeleteBtn(true)}>
+          <RiDeleteBin6Line style={{marginTop: '.5rem', fontSize: '1.8rem', fill: '#cd3e65'}}/>
+        </a>
       </td>
+      {deleteBtn && <DeleteEntity setOpen={setDeleteBtn} open={deleteBtn} entity='user'/>}
     </tr>
   );
 }
