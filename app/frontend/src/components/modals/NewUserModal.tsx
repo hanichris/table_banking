9d6958fb93fce99ee3246f298db528e32857f51c
hanichris <hanichris71@gmail.com>
2023-10-27 21:51:05 +0000

    Update `NewUserModal` component with appropriate form fields.

--- app/frontend/src/components/modals/NewUserModal.tsx
+++ app/frontend/src/components/modals/NewUserModal.tsx
@@ -17,32 +17,44 @@ export default function NewUser({setOpen, open, title}: StateEntityProp) {
             <form action="" method="post">
               <div className="modal-body">
                 <div className="input-group">
-                  <label htmlFor="new-bank--title">Title</label>
+                  <label htmlFor="new-user--email">Email</label>
                   <input
                   autoFocus
-                  id="new-bank--title"
+                  id="new-user--email"
                   type="text"
-                  name="title"
-                  placeholder="New bank title"/>
-                </div>
-                <div className="input-group _m-b-0">
-                  <label htmlFor="new-bank--interest-rate">Interest rate</label>
-                  <input
-                  id="new-bank--interest-rate"
-                  type="number"
-                  name="interest_rate"
-                  min={0}
-                  max={0.1}
-                  step={0.01}
-                  defaultValue={0.00}
+                  name="user_email"
+                  placeholder="john@email.com"
+                  required
                   />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="new-user--password">Password</label>
+                  <input
+                  id="new-user--password"
+                  type="password"
+                  name="user_pwd"
+                  placeholder="password"
+                  required
+                  />
+                </div>
+                <div className="input-group admin-input _m-b-0">
+                  <div>
+                    <label htmlFor="new-user--isAdmin">Admin</label>
+                    <input
+                      id="new-user--isAdmin"
+                      type="checkbox"
+                      name="is_superuser"
+                      value='true'
+                    />
+                  </div>
+                  <input type="hidden" name='not_superuser' value='false' />
                 </div>
               </div>
               <div className="modal-footer">
-                  <button type="submit" className="btn btn--m btn--primary">
-                    Save
-                  </button>
-                </div>
+                <button type="submit" className="btn btn--m btn--primary">
+                  Save
+                </button>
+              </div>
             </form>
           </div>
         </div>
