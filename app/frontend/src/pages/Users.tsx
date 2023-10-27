21d121149d022241d4a1f9a485cf420b8b7fc4ab
hanichris <hanichris71@gmail.com>
2023-10-27 21:53:33 +0000

    fix: display component when no user was found.

--- app/frontend/src/pages/Users.tsx
+++ app/frontend/src/pages/Users.tsx
@@ -1,14 +1,33 @@
 import { useAsyncValue } from "react-router-dom";
-import { IUser } from "../interfaces";
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2'
+
+import { I404, IUser } from "../interfaces";
 import UserExcerpt from "./UserExcerpt";
 
-export default function Users() {
-  const users = useAsyncValue() as Array<IUser> | IUser;
+export default function Users({ clear }: {clear: () => void}) {
+  const users = useAsyncValue() as Array<IUser> | IUser | I404;
   if (Array.isArray(users)) {
     return (
       users.map((user) => (
       <UserExcerpt key={user.id} user={user}/>
       ))
+    );
+  }
+  if ("detail" in users) {
+    return (
+      <tr>
+        <td className="no-results">
+          <div className="no-results__container">
+            <div>
+              <HiOutlineMagnifyingGlass />
+              <span>No user found.</span>
+              <a className="link link--primary" onClick={clear}>
+                Clear filters
+              </a>
+            </div>
+          </div>
+        </td>
+      </tr>
     );
   }
   return (
