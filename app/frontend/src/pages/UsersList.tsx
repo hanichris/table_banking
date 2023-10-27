8d826f47de1b31a2ef8200a54b99b86f395bf6b0
hanichris <hanichris71@gmail.com>
2023-10-27 18:02:25 +0000

    Create a Skeleton fallback. Enable searching a list of users by their ID.

--- app/frontend/src/pages/UsersList.tsx
+++ app/frontend/src/pages/UsersList.tsx
@@ -1,17 +1,88 @@
-import { Suspense } from "react";
-import { Await, useLoaderData } from "react-router-dom";
+import React, { Suspense, useState, useRef} from "react";
+import { Await, useLoaderData, Form, useSubmit, useNavigate } from "react-router-dom";
+import { BiPlus } from "react-icons/bi";
+import { FiSearch } from 'react-icons/fi';
+import { GiCancel } from 'react-icons/gi';
+
 import { IData } from "../interfaces";
 import Users from "./Users";
+import UsersSkeleton from "../components/skeletons/UsersSkeleton";
+
 
 export default function UsersListing() {
-  // const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
+  const [hasSearch, setHasSearch] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submit = useSubmit();
+  const navigate = useNavigate();
   const users = useLoaderData() as IData;
+
+  const handleSearchChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const clearSearch = () => {
+    if (hasSearch) {
+      setHasSearch(false);
+    }
+    setSearch('');
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+    navigate('.');
+  }
+
   return (
-    <div id="users-listing">
+    <div id="users-listing" className={hasSearch === true ? 'has-search': undefined}>
       <header className="users-listing__top">
         <h2 className="users-listing__title">
           Users
         </h2>
+        <div className="users-listing__filters">
+          <div className="users-listing__search">
+            <Form id='search-form' role="search">
+              <input
+                ref={inputRef}
+                type="search"
+                name="id"
+                placeholder="Search user id"
+                defaultValue={users.userID || search}
+                onChange={handleSearchChange}
+                onBlur={(e) => {
+                  if (search.length > 0) {
+                    submit(e.currentTarget.form, {
+                      replace: true,
+                    });
+                  }
+                  if (hasSearch && search.length === 0) {
+                    setHasSearch(false);
+                  }
+                }}
+                />
+                <span className="input-search__search-btn">
+                  <FiSearch />
+                </span>
+                <span className="input-search__clear-search" onClick={clearSearch}>
+                  <GiCancel />
+                </span>
+            </Form>
+          </div>
+          <button id='banklist-new_bank-btn' className="btn btn--s btn--secondary">
+            <span>New</span>
+            <BiPlus />
+          </button>
+          <button id='mobile__search-btn' className="btn btn--s btn--icon btn--secondary">
+            <i className="icon">
+              <FiSearch />
+            </i>
+          </button>
+          <button id='mobile__new-btn' className="btn btn--s btn--icon btn--secondary">
+            <i className="icon">
+              <BiPlus />
+            </i>
+          </button>
+        </div>
       </header>
       <div className="users-listing__body">
         <table>
@@ -27,10 +98,10 @@ export default function UsersListing() {
             </tr>
           </thead>
           <tbody>
-            <Suspense fallback={<p>Loading users table...</p>}>
+            <Suspense fallback={<UsersSkeleton />}>
               <Await
                 resolve={users.data}
-                errorElement={<p>Error loading users!</p>}>
+                errorElement={<tr><td>Error loading users!!!</td></tr>}>
                   <Users />
                 </Await>
             </Suspense>
