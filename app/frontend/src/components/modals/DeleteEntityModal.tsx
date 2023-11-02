0351829ce2d6753de8b0948ed8b82d86b769b35e
hanichris <hanichris71@gmail.com>
2023-11-02 18:29:07 +0000

    Make the DeleteEntityModal more generic. Handles the case where the entity is either a `bank` or `user`

--- app/frontend/src/components/modals/DeleteEntityModal.tsx
+++ app/frontend/src/components/modals/DeleteEntityModal.tsx
@@ -1,27 +1,41 @@
+import { useFetcher, useNavigate } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
+
 import { DeleteEntityProp } from "../../interfaces";
 
-export default function DeleteEntity({setOpen, open, entity}: DeleteEntityProp) {
+export default function DeleteEntity({setOpen, open, entity, text}: DeleteEntityProp) {
+  const fetcher = useFetcher();
+  const navigate = useNavigate();
+
+  const handleDeleteBtnClick = () => {
+    fetcher.submit({}, {
+      method: 'DELETE',
+      action: `${entity.id}/destroy`
+    });
+    setOpen(false);
+    navigate('.');
+  }
+
   return (
     <section className="modal" id="confirm" style={open ? {display: 'block'} : {}}>
       <div className="modal-container">
         <div className="modal-cell">
           <div className="modal-element" style={open ? {position:'relative', transform: 'translate3d(0, 0, 0) scale(1)', opacity:'1'}: {}}>
             <header className="modal-header">
-              <div className="modal-header__title">Delete {entity}</div>
+              <div className="modal-header__title">Delete {text}</div>
               <a className="modal_button-left btn btn--icon btn--xs btn--transparent" onClick={() => setOpen(false)}>
                 <RiCloseLine />
               </a>
             </header>
             <div className="modal-body" id="confirm-text">
-              <div>Do you really want to delete this {entity}?</div>
+              <div>Do you really want to delete this {text}?</div>
             </div>
             <div className="modal-footer">
               <div className="modal-footer-btns">
                 <button type="button" className="btn btn--m btn--secondary" onClick={() => setOpen(false)}>
                   <span>Cancel</span>
                 </button>
-                <button type="button" className="btn btn--m btn--danger">
+                <button type="button" className="btn btn--m btn--danger" onClick={handleDeleteBtnClick}>
                   <span>Delete</span>
                 </button>
               </div>
