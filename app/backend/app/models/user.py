31dc214aa4b5ad17386bc2e74858fdc8f04278be
hanichris <hanichris71@gmail.com>
2023-12-14 21:45:18 +0000

    Update: Add `created_at` and `updated_at` columns.

--- app/backend/app/models/user.py
+++ app/backend/app/models/user.py
@@ -6,6 +6,9 @@ within the database table to be generated. The construct utilises
 the SQLAlchemy type `Mapped` to derive its column-configuration
 information.
 """
+import datetime
+
+from sqlalchemy import func, FetchedValue
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 
@@ -16,6 +19,8 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     full_name: Mapped[str | None] = mapped_column(index=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(onupdate=func.now(), server_default=func.now(), server_onupdate=FetchedValue())
     password: Mapped[str] = mapped_column()
     is_active: Mapped[bool] = mapped_column(insert_default=True)
     is_superuser: Mapped[bool] = mapped_column(insert_default=False)
