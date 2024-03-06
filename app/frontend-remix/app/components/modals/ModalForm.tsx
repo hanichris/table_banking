500ffaf01312c244980c5fe2d9e1082795a06c06
hanichris <hanichris71@gmail.com>
2024-03-06 21:48:12 +0000

    Integrate the sign in modal component with the parent SignInSignUp component

--- app/frontend-remix/app/components/modals/ModalForm.tsx
+++ app/frontend-remix/app/components/modals/ModalForm.tsx
@@ -1,8 +1,11 @@
-import { IModalProp } from "~/interfaces";
 import UserForm from "./UserForm";
+import SignIn from "./SignInForm";
+
+import { IModalProp } from "~/interfaces";
 
 export const SignInSignUp = (props: IModalProp) => (
   <>
     <UserForm {...props}/>
+    {props.state.status === "signIn" && <SignIn {...props}/>}
   </>
 );
