835b8f23f420c418a0b31215c342c88f1aa5af04
hanichris <hanichris71@gmail.com>
2024-03-06 11:57:38 +0000

    Obtain the dimensions of the window to appropriately render the required components.

---
+++ app/frontend-remix/app/components/navbar/useDimensions.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function getWindowDimensions(){
+  const {innerHeight: height, innerWidth: width} = window;
+  return {
+    height,
+    width,
+  };
+}
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowDimensions;
+};
