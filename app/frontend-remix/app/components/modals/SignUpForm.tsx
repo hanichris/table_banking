4f82fd20535530dc65358298fcc1c8d9313d85be
hanichris <hanichris71@gmail.com>
2024-03-09 20:55:08 +0000

    Enhance the html form using remix's `<Form>` tag together with the useNavigation and useActionData hooks.

--- app/frontend-remix/app/components/modals/SignUpForm.tsx
+++ app/frontend-remix/app/components/modals/SignUpForm.tsx
@@ -1,12 +1,19 @@
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import { useState } from "react";
 import { RiCloseLine } from "react-icons/ri";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 
 import { IModalProp } from "~/interfaces";
+import { Spinner } from "../Spinner";
+import { action } from "~/root";
 
 export default function SignUp({toggleForm, state}: IModalProp) {
   const [pwdType, setPwdType] = useState("password");
+  const navigation = useNavigation();
+  const actionData = useActionData<typeof action>();
+
   const open = state.status === "signUp" && !state.isRendered;
+  const isSubmitting = navigation.state === "submitting";
 
   const togglePwdType = () => pwdType === "password" ? setPwdType("text") : setPwdType("password");
 
@@ -14,7 +21,7 @@ export default function SignUp({toggleForm, state}: IModalProp) {
     <section className="modal" style={open ? {display: "block"} : {display: "none"}}>
       <div className="modal-container">
         <div className="modal-cell">
-          <div className="modal-element">
+          <div className="modal-element" style={open ? {position: "relative", opacity: 1, transform: "translate3d(0, 0, 0) scale(1"} : {}}>
             <header className="modal-title">
               <h2>Sign Up</h2>
               <div>Create a free account with your email</div>
@@ -23,9 +30,10 @@ export default function SignUp({toggleForm, state}: IModalProp) {
               <RiCloseLine />
             </button>
             <div className="modal-body">
-              <form action="" method="post">
+              <Form action="" method="post">
                 <fieldset>
                   <legend className="visually-hidden">Your personal information</legend>
+                  <input type="hidden" name="auth" value="signup"/>
                   <div className="form-group">
                     <label htmlFor="your-full_name">Full Name</label>
                     <div className="form-field">
@@ -37,6 +45,8 @@ export default function SignUp({toggleForm, state}: IModalProp) {
                           placeholder="Full Name"
                           maxLength={100}
                           autoComplete="false"
+                          disabled={isSubmitting}
+                          className={isSubmitting ? "disabled": ""}
                           required />
                           <i className="form-field-icon"></i>
                           <p className="form-help">Full name should be at least 3 characters and only contain letters</p>
@@ -54,6 +64,8 @@ export default function SignUp({toggleForm, state}: IModalProp) {
                           placeholder="john@email.com"
                           pattern="[\w!#$%&'*+\/=?`\{\|\}~^\-]+(?:\.[\w!#$%&'*+\/=?`\{\|\}~^\-])*@(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}"
                           maxLength={55}
+                          disabled={isSubmitting}
+                          className={isSubmitting ? "disabled": ""}
                           required />
                           <i className="form-field-icon"></i>
                           <p className="form-help">Email should be a valid email address that matches the RFC standard</p>
@@ -70,6 +82,8 @@ export default function SignUp({toggleForm, state}: IModalProp) {
                           id="your-password"
                           placeholder="password"
                           minLength={8}
+                          disabled={isSubmitting}
+                          className={isSubmitting ? "disabled": ""}
                           required />
                           <a className="pwd-icon" onClick={togglePwdType}>
                             {pwdType === "password" ? <VscEye /> : <VscEyeClosed />}
@@ -77,11 +91,15 @@ export default function SignUp({toggleForm, state}: IModalProp) {
                       </span>
                     </div>
                   </div>
-                  <button type="submit" className="btn btn--m btn--primary">
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className={isSubmitting ? "btn btn--m btn--primary is-disabled" : "btn btn--m btn--primary"}>
                     Create your free account
+                    {isSubmitting && <Spinner />}
                   </button>
                 </fieldset>
-              </form>
+              </Form>
               <div className="small-text _m-t-3">
                 By continuing you agree to our <a className="link link--black">Terms of Service.</a>
                 <br />
