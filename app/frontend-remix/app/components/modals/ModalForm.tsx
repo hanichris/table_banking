77518e930895b91f2f39d8569655b72145d3abe6
hanichris <hanichris71@gmail.com>
2024-03-09 20:19:49 +0000

    interface the sign up component with the parent component `SignInSignUp`

--- app/frontend-remix/app/components/modals/ModalForm.tsx
+++ app/frontend-remix/app/components/modals/ModalForm.tsx
@@ -1,11 +1,13 @@
 import UserForm from "./UserForm";
 import SignIn from "./SignInForm";
+import SignUp from "./SignUpForm";
 
-import { IModalProp } from "~/interfaces";
+import { IModalProp } from "../../interfaces";
 
 export const SignInSignUp = (props: IModalProp) => (
   <>
     <UserForm {...props}/>
     {props.state.status === "signIn" && <SignIn {...props}/>}
+    {props.state.status === "signUp" && <SignUp {...props}/>}
   </>
 );
